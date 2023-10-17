f6b348acd0bed5db905ddb3981aec5e94685baca
Joonas Venäläinen <joonvena@gmail.com>
2023-10-17 18:15:44 +0300

    Fix lint errors and remove extractor for now.

--- config/mysql/config.go
+++ config/mysql/config.go
@@ -2,10 +2,8 @@ package mysql
 
 import "github.com/upbound/upjet/pkg/config"
 
-const (
-	ConfigPackagePath = "github.com/joonvena/provider-aiven/config"
-)
-
+// Configure configures individual resources by adding custom
+// ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aiven_mysql_database", func(r *config.Resource) {
 		r.References["service_name"] = config.Reference{
